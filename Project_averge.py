270844a4c47db3b528b415461c1bec0e03f1aa74
Donyafallah <donyafallah2004@gmail.com>
2024-07-28 15:37:51 +0330

    We recalculated the average grade for each student and sorted them in ascending order, prioritizing the average grade first and then alphabetical order. Then, we identified the top three students using the same criteria.

--- Project_averge.py
+++ Project_averge.py
@@ -1,5 +1,6 @@
 import csv
 from statistics import mean
+from collections import OrderedDict
 
 def calculate_averages(input_file_name, output_file_name):
     with open(input_file_name , 'r') as f :
@@ -14,6 +15,48 @@ def calculate_averages(input_file_name, output_file_name):
                 list_grades.append(float(grade))
             writer.writerow([name , mean(list_grades)])
 
+def calculate_sorted_averages(input_file_name, output_file_name):  
+    dictionary = OrderedDict()          
+    with open(input_file_name , 'r') as f :
+        reader = csv.reader(f)
+        rows = list(reader)
+    with open(output_file_name , 'w' , newline='') as k :
+        writer = csv.writer(k)
+        for row in rows :
+            name = row[0]
+            list_grades = list()
+            for grade in row[1:] :
+                list_grades.append(float(grade))
+            dictionary[name] = mean(list_grades)
+            sorted_dictionary = OrderedDict(sorted(dictionary.items() , key= lambda item : (item[1] , item[0])))
+            list_keys = list(sorted_dictionary.keys())
+            list_values = list(sorted_dictionary.values())
+        for i in range(0 , len(list_keys)) :
+            writer.writerow([list_keys[i] , sorted_dictionary[list_keys[i]]])    
+
+
+def calculate_three_best(input_file_name, output_file_name):
+    dictionary = OrderedDict()
+    with open(input_file_name , 'r') as f :
+        reader = csv.reader(f)
+        rows = list(reader)
+    with open(output_file_name , 'w' , newline='') as k :
+        writer = csv.writer(k)
+        for row in rows :
+            name = row[0]    
+            list_grades = []
+            for grade in row[1:] :
+                list_grades.append(float(grade))
+            dictionary[name] = mean(list_grades)    
+            sorted_dictionary = OrderedDict(sorted(dictionary.items() , key= lambda item : (-item[1] ,item[0])))
+            list_keys = list(sorted_dictionary.keys())
+            list_values = list(sorted_dictionary.values())
+        for i in range (3) :
+            writer.writerow([ list_keys[i] , sorted_dictionary[list_keys[i]]])
+
+
 if __name__ == "__main__" :   
     input_file = "grades.csv"
     calculate_averages( input_file , "averages.csv")
+    calculate_sorted_averages( input_file , "sorted_averages.csv")
+    calculate_three_best( input_file , "three_best_avrages")
